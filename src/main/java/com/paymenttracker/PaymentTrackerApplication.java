740a61c33db1019d42c0e8d052e8294369130bc2
Anuroop0333 <anuroopjajoba3@gmail.com>
2026-03-28 16:27:12 -0400

    Initial project setup: Spring Boot 3.2, Java 25, Maven dependencies

---
+++ src/main/java/com/paymenttracker/PaymentTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.paymenttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentTrackerApplication.class, args);
+    }
+}
